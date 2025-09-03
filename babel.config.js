a990c57547bb8a927eb224839870fdada9b3d11e
GonzaloIRC <gonzaloignacio.r.c@gmail.com>
2025-09-03 19:35:38 -0400

    M0 repair: configuración correcta de babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 ﻿module.exports = function (api) {
   api.cache(true);
   return {
+    sourceType: 'unambiguous',
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      ['module-resolver', { root: ['./'], alias: { '@': './src' } }],
+      'react-native-reanimated/plugin', // SIEMPRE el último
+    ],
   };
 };
